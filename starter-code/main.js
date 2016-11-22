2c30bb9965bffb62153ef8b183420851fa167315
J Riley <jlr7245@gmail.com>
2016-11-21 22:02:00 -0500

    finished game

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,49 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo){
-  alert('You found a match!');
-} else {
-  alert('Whoops! Try again');
+
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+  for (var i = 0; i < 4; i++) {
+  var newCard = document.createElement('div');
+  newCard.className = 'card';
+  gameBoard.appendChild(newCard);
+}
+};
+
+createCards();
+
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var createBoard = function() {
+  for (var i=0; i < cards.length; i++) {
+    document.querySelectorAll(".card")[i].setAttribute('data-card', cards[i]);
+  }
+};
+
+createBoard();
+
+for (var i=0; i<cards.length; i++) {
+  document.querySelectorAll(".card")[i].addEventListener('click', isTwoCards);
+}
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if(cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    return alert('good job!');
+  }
+  else {
+    return alert('Try again!');
+  }
 }
